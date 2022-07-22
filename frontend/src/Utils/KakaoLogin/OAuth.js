8bd38ea2a42dab791c485a0eaee6436be8613646
asegh12 <lgh009833@kookmin.ac.kr>
2022-07-22 17:40:24 +0900

    add: 카카오 로그인 시, 로직 추가

---
+++ frontend/src/Utils/KakaoLogin/OAuth.js
@@ -0,0 +1,6 @@
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+export const KAKAO_AUTH_TOKEN_URL = `https://kauth.kakao.com/oauth/token?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&`;
---
+++ frontend/src/Utils/KakaoLogin/OAuth2RedirectHandler.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { getLogin } from "apis/auth";
+import { getMyProfile, join } from "apis/user";
+import { KAKAO_AUTH_TOKEN_URL } from "./OAuth";
+import { KAKAO_ACCESS_TOKEN, API_TOKEN } from "../localStorageKeys";
+
+// 추가적으로 해야할 작업
+// 1. 로그인에 따라서 상태 업데이트
+// 2. 후에 헤더에서 로그인 헤더로 보여줘야함.
+function OAuth2RedirectHandler() {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const code = searchParams.get("code");
+		const getTokenURL = `${KAKAO_AUTH_TOKEN_URL}code=${code}&grant_type=authorization_code`;
+
+		// 카카오에서 엑세스 토큰 얻어오기
+		const getToken = async () => {
+			await axios({
+				method: "post",
+				url: getTokenURL,
+				headers: {
+					"content-type": "application/x-www-form-urlencoded;charset=utf-8",
+				},
+			})
+				// 정상적으로 받아올 경우
+				.then((res) => {
+					localStorage.setItem(KAKAO_ACCESS_TOKEN, res.data.access_token);
+					const tryLogin = async () => {
+						// 로그인 시도하기
+						await getLogin(res.data.access_token)
+							// 로그인 성공시 2가지 경우
+							.then((res) => {
+								localStorage.setItem(API_TOKEN, res.data.response.api_token);
+								// !!! 바로 아래 myprofile 로직은 바꿔야할 필요가 있다
+								const myprofile = async () => {
+									await getMyProfile(res.data.response.api_token)
+										// 1. 닉네임이 없는 경우 mypage 이동
+										// 2. 닉네임 있는 경우는 landingPage 이동 -> 기존 있던 페이지로 이동하게 할 필요가 있다.
+										.then((res) => {
+											if (!res.data.response.nickname) navigate("/mypage");
+											else navigate("/");
+										})
+										.catch((err) => {
+											console.log(err);
+										});
+								};
+								myprofile();
+							})
+							// 로그인 실패시 회원가입하고 로그인
+							.catch((err) => {
+								if (err.data.status === 401) {
+									const getJoin = async () => {
+										await join(res.data.access_token)
+											.then((res) => {
+												navigate("/mypage");
+											})
+											.catch((err) => {
+												console.log(err);
+											});
+									};
+									getJoin();
+								}
+							});
+					};
+					tryLogin();
+				})
+				// 에러가 있을 경우
+				.catch((err) => console.log(err));
+		};
+
+		getToken();
+	}, []);
+
+	return <div style={{ display: "hidden" }} />;
+}
+
+export default OAuth2RedirectHandler;
