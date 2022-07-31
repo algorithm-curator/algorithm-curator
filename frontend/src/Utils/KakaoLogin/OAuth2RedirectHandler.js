9523949f8127b61541c4ffea48404eedad8a7f1c
asegh12 <lgh009833@kookmin.ac.kr>
2022-07-31 13:09:07 +0900

    fix: warning 해결

--- frontend/src/Utils/KakaoLogin/OAuth2RedirectHandler.js
+++ frontend/src/Utils/KakaoLogin/OAuth2RedirectHandler.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
