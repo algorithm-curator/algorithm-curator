f9395b6161cbbc9220e494ab00d9bbf7e1d758da
asegh12 <lgh009833@kookmin.ac.kr>
2022-08-11 13:42:51 +0900

    update: REDIRECT_URL 수정

--- frontend/src/Utils/KakaoLogin/OAuth.js
+++ frontend/src/Utils/KakaoLogin/OAuth.js
@@ -1,9 +1,5 @@
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-<<<<<<< HEAD
-const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
-=======
 const REDIRECT_URI = "https://apcurator.net/oauth/callback/kakao";
->>>>>>> 1aec92c (add: rankings API 연동하고 로직 작성 / warning 해결)
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
