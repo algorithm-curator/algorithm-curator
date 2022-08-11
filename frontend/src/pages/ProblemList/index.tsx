5e7fc06123685de3498552bf17b8247ded1ea40e
asegh12 <lgh009833@kookmin.ac.kr>
2022-08-11 12:22:17 +0900

    update: 무한스크롤 하지 않고 더보기 버튼으로 처리

--- frontend/src/pages/ProblemList/index.tsx
+++ frontend/src/pages/ProblemList/index.tsx
@@ -1,34 +1,30 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import ReactLoading from "react-loading";
 import ProblemAllList from "components/problem/ProblemAllList";
 import ProblemListTab from "components/problem/ProblemListTab";
 import { getListProblems } from "apis/problem";
 import { API_TOKEN } from "Utils/localStorageKeys";
-import { ChartLink, Container, Title, TitleChartWrapper } from "./styles";
+import {
+	ChartLink,
+	Container,
+	Title,
+	TitleChartWrapper,
+	MoreButton,
+} from "./styles";
 
 function ProblemList() {
 	const apiToken = localStorage.getItem(API_TOKEN);
 	const [problems, setProblems] = useState<any[]>();
 	const [page, setPage] = useState<number>(1);
 	const [filterStatus, setFilterStatus] = useState<number | null>(null);
-	const boxRef = useRef(null);
-	const observerRef = React.useRef<IntersectionObserver>();
 	const getProblems = async (
 		paging: boolean,
 		state: number | null,
 		stateChange: boolean
 	) => {
-		console.log(
-			"paging :",
-			paging,
-			"| state :",
-			state,
-			"| stateChange :",
-			stateChange
-		);
 		// paging이 true면 페이징 하기
 		if (stateChange) setPage(1);
 		// eslint-disable-next-line no-nested-ternary
@@ -46,24 +42,9 @@ function ProblemList() {
 				// console.log(err);
 			});
 	};
-	const intersectionObserver = (entries: any, io: any) => {
-		entries.forEach((entry: any) => {
-			if (entry.isIntersecting) {
-				// 관찰하고 있는 entry가 화면에 보여지는 경우
-				io.unobserve(entry.target); // entry 관찰 해제
-				getProblems(true, filterStatus, false); // 데이터 가져오기
-			}
-		});
-	};
 	useEffect(() => {
 		getProblems(false, null, false);
 	}, []);
-
-	useEffect(() => {
-		observerRef.current = new IntersectionObserver(intersectionObserver);
-		// eslint-disable-next-line no-unused-expressions
-		boxRef.current && observerRef.current.observe(boxRef.current);
-	}, [problems]);
 
 	return (
 		<Container>
@@ -76,7 +57,7 @@ function ProblemList() {
 				getProblems={getProblems}
 			/>
 			{problems ? (
-				problems.map((problem: any, index) => {
+				problems.map((problem: any) => {
 					return (
 						<ProblemListTab
 							key={problem.id}
@@ -99,7 +80,25 @@ function ProblemList() {
 					<ReactLoading type="spin" width="5%" />
 				</div>
 			)}
-			{problems ? <div ref={boxRef} /> : null}
+			<div
+				style={{
+					width: "100%",
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					marginTop: "1rem",
+				}}
+			>
+				{problems ? (
+					<MoreButton
+						onClick={(e) => {
+							getProblems(true, filterStatus, false);
+						}}
+					>
+						더보기
+					</MoreButton>
+				) : null}
+			</div>
 		</Container>
 	);
 }
